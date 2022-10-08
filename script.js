69f78660fca13b38ed5dbca6f20d933301a598b4
nandini27ns <nandiniiii27@gmail.com>
2022-10-09 03:13:56 +0530

    6 commit

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const hamburger=document.querySelector(".hamburger");
 const navvy=document.querySelector(".navvy");
+const right=document.querySelector(".right");
 
 
 hamburger.addEventListener("click",()=>{
     hamburger.classList.toggle("active");
     navvy.classList.toggle("active");
-
+    right.style.display=="flex"?right.style.display="none":right.style.display="flex";
 })
 
 document.querySelectorAll("nav-link").forEach(n => n.addEventListener("click",()=>{
